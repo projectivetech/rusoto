b70477c5567f24b997a3efcb947973b7794fac15
Steve Dierker <steve.dierker@projective.io>
2024-10-17 13:49:33 +0200

    Add a flake for dependencies and Rust version.
    Flake for dependencies and rust version.

---
+++ flake.nix
@@ -0,0 +1,24 @@
+# in flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [rust-bin.stable."1.70.0".default  rust-analyzer openssl.dev pkg-config];
+          };
+        }
+      );
+}
